ea5e645b8228968fd2d2e8fb7a70349a43dc384f
ChenYFan <chen2778754364@foxmail.com>
2021-05-18 20:19:53 +0800

    尝试新的样式

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import init from 'raw-loader!./html/init.html'
+import css from 'raw-loader!./../dist/htalk.css'
+console.log(init)
+console.log(css)
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  purge: {
+    enabled: true,
+    content: [
+      './index.html'
+    ]
+  }
+}
